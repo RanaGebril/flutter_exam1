7ac5989e9f2c7875f52bd06e38a1e7440669bd03
Rana_Gebril <ranagebril5@gmail.com>
2024-08-02 22:53:32 +0300

    bottom_container update

---
+++ lib/screen2/bottom_container.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BottumContainer extends StatelessWidget {
+  Color bg;
+  String label;
+  String image;
+  BottumContainer({required this.bg,required this.label,required this.image,super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      width: 151,
+      height: 56,
+      decoration:BoxDecoration(
+        color: bg,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        children: [
+          Image.asset(image,height: 24,width: 24),
+          SizedBox(
+            width: 5,
+          ),
+          Text(label,
+            style: GoogleFonts.inter(
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
+                color: Color(0xff000000)
+            ),)
+        ],
+      ),
+
+
+    );
+  }
+}
